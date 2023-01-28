f26b3ca7c4a5b98e537cb17b7b2ec5752ef07beb
jknox927 <joshua.knox927@gmail.com>
2023-01-28 00:59:43 +0000

    Init commit, not all parts work as intended...

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { SecondsCounter } from "./SecondsCounter.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [counter, setCounter] = useState(0);
+    const [counterStr, setCounterStr] = useState(counter.toString().padStart(6, "0"));
+    const [countDown, setCountDown] = useState(0);
+    const [alertTarget, setAlertTarget] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (!countDown) {
+                setCounter((prevCounter) => prevCounter + 1);
+                setCounterStr(counter.toString().padStart(6, "0"));
+            }
+            else {
+                setCountDown(countDown - 1);
+                setCounter((countDown) => countDown - 1);
+                setCounterStr(countDown.toString().padStart(6, "0"));
+            }
+            if (alertTarget && counter === alertTarget) {
+                    alert("Number Reached!");
+            }
+
+        }, 1000);
+
+    }, [counter]);
+
+	
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<SecondsCounter counterStr = {counterStr} />
+			<input className="input" placeholder="Enter a Number" type="number" onChange={e => setCountDown(parseInt(e.target.value))} />
+            <input className="input" placeholder="Alert" type="number" onChange={e => setAlertTarget(parseInt(e.target.value))} />
+            <div className="btn-container">
+                <button className="buttons">Stop</button>
+                <button className="buttons">Reset</button>
+                <button className="buttons">Resume</button>
+            </div>
 		</div>
 	);
 };
